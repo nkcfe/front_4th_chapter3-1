670bb7aa32e94eed20b5631a05b0ca8890f47174
nkcfe <chul.develop@gmail.com>
2025-02-06 03:16:42 +0900

    Feat: 테스트 작성을 위한 기본 세팅 추가

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+/// <reference types="vitest" />
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    css: true,
+  },
+});
